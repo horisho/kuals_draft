56ac30f899644c469089e7088c37b403fff8eb29
horisho <oh.yes.i.know@gmail.com>
2018-10-16 11:53:46 +0900

    write seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,9 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
 User.create!(name: "test", password:"testest", password_confirmation: "testest")
+
+99.times do |n|
+  name = "user#{n}"
+  password = "password"
+  User.create!(name: name, password: password)
+end
