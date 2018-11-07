40d77b93a2714a7cc1cfc373cb180cae987632c5
horisho <oh.yes.i.know@gmail.com>
2018-11-07 21:11:26 +0900

    maintenance

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "登録完了だにょ"
       redirect_to root_url
     else
